d649d587ce95671c6a7c025c9dde4a4bd48da749
leonardom249 <leonardo.megan@gmail.com>
2018-05-24 14:24:46 -0500

    finished bucketsort

--- sorting.js
+++ sorting.js
@@ -25,8 +25,8 @@ function partition(arr, start, end){
 }
 const arr=[89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
 
-console.log(qSort(arr));
-console.log('count:', count);
+// console.log(qSort(arr));
+// console.log('count:', count);
 
 function swap(array, i, j) {
   const tmp = array[i];
@@ -70,5 +70,24 @@ function merge(left, right, arr){
   }
   return arr;
 }
-console.log(mSort(arr));
-console.log('mSort count:',mCount);
+// console.log(mSort(arr));
+// console.log('mSort count:',mCount);
+
+function bucketSort(arr, lowest, highest){
+  const newArr= new Array((highest-lowest)+1);
+  let finalArr=[];
+
+  for(let i=0; i<arr.length; i++){
+    newArr[arr[i]-lowest]=arr[i];
+  }
+
+  for(let i=0; i<newArr.length; i++){
+    if(typeof newArr[i]==='number'){
+      finalArr.push(newArr[i]);
+    }
+  }
+  return finalArr;
+  
+}
+//removes duplicates: extension should be see how to handle duplicates more efficiently
+console.log('bucket', bucketSort(arr, 1, 98));
